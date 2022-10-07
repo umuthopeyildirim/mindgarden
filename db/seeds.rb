789c3f30ce66c6fa6bce0cb13e29b73b2490e642
Umut Hope YILDIRIM <umut475@gmail.com>
2022-10-07 01:39:24 -0400

    Feelings Friday?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,43 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+# Seed Feelings in the database
+feelings = [
+    "😀 Happy",
+    "😲 Suprised",
+    "😤 Angry",
+    "😍 In Love",
+    "😵‍💫 Frustrated",
+    "😭 Sad",
+    "🤫 Secretive",
+    "😱 Depressed",
+    "😴 Sleepy",
+    "🤪 Crazy",
+    "🤔 Confused",
+    "😡 Mad",
+    "😎 Cool",
+    "😇 Innocent",
+    "😈 Evil",
+    "🤑 Rich",
+    "🤢 Disgusted",
+    "🤮 Sick",
+    "🤡 Clown",
+    "🤠 Cowboy",
+    "🤥 Lying",
+    "🤫 Shushing",
+    "🤭 Pretending",
+    "🤐 Zipped",
+    "🤓 Nerd",
+    "🤗 Hugging",
+    "🤣 Funny",
+    "🤯 Burned Out",
+    "🤒 Sick",
+    "😶 Bored",
+    "😷 Sick",
+]
+
+feelings.each do |feeling|
+    Feeling.create(name: feeling)
+end
+
 
 puts "✅ Done seeding!"
